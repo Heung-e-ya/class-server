49dedc03d770744bc76985502f94694a63d116da
heung <hoonun@gmail.com>
2022-04-11 19:09:42 +0900

    express에서 데이터 처리하기

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/products", (req, res) => {
+  const query = req.query;
+  console.log("QUERY :", query);
+  res.send({
+    products: [
+      {
+        id: 1,
+        name: "농구공",
+        price: "100000",
+        seller: "조던",
+        imageUrl: "../images/images/products/basketball1.jpeg",
+      },
+      {
+        id: 2,
+        name: "축구공",
+        price: "200000",
+        seller: "메시",
+        imageUrl: "../images/images/products/soccerball1.jpg",
+      },
+      {
+        id: 3,
+        name: "키보드",
+        price: "15000",
+        seller: "구랩",
+        imageUrl: "../images/images/products/keyboard1.jpg",
+      },
+    ],
+  });
+});
+
+app.post("/products", (req, res) => {
+  const body = req.body;
+  res.send({
+    body,
+  });
+});
+
+app.get("/products/:id/events/:eventsId", (req, res) => {
+  const params = req.params;
+  const { id, eventsId } = params;
+  res.send(`id는 ${id}와 ${eventsId} 입니다.`);
+});
+
+app.listen(port, () => {
+  console.log("그랩에 쇼핑몰 서버가 돌아가고 있습니다.");
+});
